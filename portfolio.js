dfbb396d14a2a1f9d80961db51644e90aeb5801f
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-08-17 08:57:44 -0700

    rendered

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,25 @@
 'use strict';
-function Experience (type, dates, description, value) {
-  this.type = type;
-  this.dates = dates;
-  this.description = description;
-  this.value = value;
+var qualities = [];
+
+function Experience (opts) {
+  this.type = opts.type;
+  this.dates = opts.dates;
+  this.description = opts.description;
+  this.value = opts.value;
 }
-Section.prototype.toHtml = function () {
+Experience.prototype.toHtml = function () {
   var $newSection = $('section.template').clone();
 
   $newSection.find('h1').text(this.type);
   $newSection.find('.dates').text(this.dates);
-  $newSection.find('description').html(this.description);
+  $newSection.find('.description').text(this.description);
   $newSection.find('.value').text(this.value);
+  $newSection.removeClass();
   return $newSection;
 };
+experience.forEach(function(currentExperience) {
+  qualities.push(new Experience (currentExperience));
+});
+qualities.forEach(function(currentQuality) {
+  $('body').append(currentQuality.toHtml());
+});
